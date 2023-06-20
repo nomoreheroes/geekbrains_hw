80e9972d225a95737c7e6b395b5fd59cbe1cd9d5
Andrei Mossakovski <andreimoss@gmail.com>
2023-06-20 17:27:47 +0300

    creating and initialisation of an array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("I'm begging, master, tell me, what size would you like the initial array to be");
+int N = Convert.ToInt32(Console.ReadLine());
+
+String ShowArray(string[] arr)
+{
+    return $"[{string.Join(',',arr)}]";
+}
+
+string[] InitArr = new string[N];
+Console.WriteLine("The array created to your glory, we just need to fill it");
+for(int i=0;i<N;i++)
+{
+    Console.WriteLine($"I'm on my knees, please, enter the {i+1} string!");
+    InitArr[i] = Console.ReadLine();
+}
+Console.WriteLine("My lord, now we are ready to do our dark and terrible deeds");
+Console.WriteLine($"HERE IS IT: {ShowArray(InitArr)}");
+
